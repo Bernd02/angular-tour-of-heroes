d848915632c0d58aec04355b5bc23205a3893b35
Bernd <141633738+Bernd02@users.noreply.github.com>
2023-09-02 21:17:48 +0200

    Done - 2.Display a list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,8 +8,10 @@ import { HEROES } from '../mock-heroes';
     styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-    hero: Hero = {
-        id: 1,
-        name: "Windstrom"
-    };
+    heroes = HEROES;
+    selectedHero?: Hero;
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
 }
